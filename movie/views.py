12078406657f2777dc2ff6756008f5b0af043658
pauliceron <pceronm@eafit.edu.co>
2023-08-03 14:25:27 -0500

    ultimo commit

--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def about(request):
+   return render(request, 'about.html')
+
 def home(request):
    # return HttpResponse('<h1>Welcome to Home Page <h1>')
    #return render(request, 'home.html')
